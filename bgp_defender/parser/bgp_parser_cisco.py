b8e77cccbc547c2f5b0f9641716e47ed7cb76de9
krlosromero <carlosromero15@gmail.com>
2019-02-09 15:49:22 +0000

    arrangiing bgp_parsers

--- bgp_defender/parser/bgp_parser_cisco.py
+++ bgp_defender/parser/bgp_parser_cisco.py
@@ -5,14 +5,16 @@ Holds the BGP Parser object
 """
 
 
-table = open('cisco-table.txt', 'r')
+table = open('../../data/cisco-table.txt', 'r')
 
 pattern = re.compile('.[0-9].{1,}')
-ip = re.compile('\W\W\s\s[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}\/[0-9]{1,3}')
-#prefer = 
+ip = re.compile(
+    '\W\W\s\s[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}\/[0-9]{1,3}')
+# prefer =
 
 for AS in table:
-        iptable = ip.findall(AS)
-        validation = pattern.findall(AS, 63)    
-        #The AS Numbers start on position = 63
-        print("    {     ","\n IP Address",iptable,"\n   }   "," \n {  ","\n AS Number",validation,"\n   }  ")  
+    iptable = ip.findall(AS)
+    validation = pattern.findall(AS, 63)
+    # The AS Numbers start on position = 63
+    print("    {     ", "\n IP Address", iptable, "\n   }   ",
+          " \n {  ", "\n AS Number", validation, "\n   }  ")
--- bgp_defender/parser/bgp_parser_junos.py
+++ bgp_defender/parser/bgp_parser_junos.py
@@ -10,29 +10,31 @@ class BgpParser:
 
 
 def main():
-    with open('././data/test_initial_data2.txt', 'r') as f:
+    with open('../../data/juniper-table.txt', 'r') as f:
         table = f.readlines()
 
     patterns = [
         re.compile(
             r'(?P<prefix>\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b\/.[0-9])\s+'),
         re.compile(r'(?P<as_path>AS path:.{1,10})'),
-        re.compile(r'(?P<next_hop_ip>\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+'),
+        re.compile(
+            r'(?P<next_hop_ip>\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+'),
         re.compile(r'(?P<med>MED.{1,2})')
     ]
 
     result_list = []
+    # list of dictionaries ( dict per BGP route entry)
     regex_dict = {}
 
     for line in table:
-            for regexp in patterns:
-                    match = re.search(regexp, line)
-                    if match:
-                            if ('prefix') in match.groupdict():
-                                    regex_dict = {'prefix': match.group('prefix')}
-                                    result_list.append(regex_dict)
-                            else:
-                                    regex_dict.update(match.groupdict())
+        for regexp in patterns:
+            match = re.search(regexp, line)
+            if match:
+                if ('prefix') in match.groupdict():
+                    regex_dict = {'prefix': match.group('prefix')}
+                    result_list.append(regex_dict)
+                else:
+                    regex_dict.update(match.groupdict())
     #                                revised="#".join(result_list)
     #                               print(revised)
 
